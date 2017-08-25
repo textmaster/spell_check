03e79e60b1e2c6452f3e4f03281b160d556afdda
Sergio Medina <sergio.medina@textmaster.com>
2017-08-25 18:58:49 +0200

    stress test api

---
+++ stress_test/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+threads=10
+connections=300
+test_length=60s
+wrk -t$threads -c$connections -d$test_length -s ./post.lua http://api.languagetool.services.textmasterstaging.com/v2/check
