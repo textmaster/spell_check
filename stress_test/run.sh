08934615b6dd8f3c6b83e1c4d2fd9f31b734e769
Sergio Medina <sergio.medina@textmaster.com>
2017-08-29 14:21:09 +0200

    remove stress_test directory

--- stress_test/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-threads=10
-connections=300
-test_length=60s
-wrk -t$threads -c$connections -d$test_length -s ./post.lua http://api.spell-check.services.textmasterstaging.com/v2/check
